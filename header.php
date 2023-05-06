924ee13d6b734f3d2706ffa65ef2bde88135cb23
RPBTM <33368185+RPBTM@users.noreply.github.com>
2023-05-06 10:19:07 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<div id="top_banner" > 
+     <div  id="emblm_1">
+        <!-- <img class="mx-auto d-block" src ="https://drive.google.com/uc?export=download&id=1hr6OXBr9LBuLxcZ8nP4VoN5RmZueM0iW" width="65px" height="90px" alt="Sri_Lanka_emb" style= "background-color: transparent;"> -->
+        
+        <img class="mx-auto d-block" src ="<?php echo base_url('/assets/login/images/LOGO.png');?>" width="65px" height="90px" alt="Sri_Lanka_emb" style= "background-color: transparent;">
+
+     </div>
+     <div id="banner_hedding">
+     <h6 class="text-center" >MINISTRY OF HEALTH, SRI LANKA</h6>
+    <h5 class="text-center" >MEDICAL OFFICERS INFORMATION MANAGEMENT SYSTEM</h5> 
+     </div>
+</div> 
+
+
+<!-- Footer -->
+<div  id="foot_banner">
+<div style="color:white;font-size:9px" class=" text-center"> MSIU, MINISTRY OF HEALTH, SRI LANKA - 2021 </div>
+</div>
